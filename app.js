fa5478078696cafa728452b321c56319f16d862d
Kyle Pace <kylepace03@gmail.com>
2013-03-12 20:26:37 -0400

    Bow tit

---
+++ app.js
@@ -0,0 +1,37 @@
+var express = require('express')
+    , app = express()
+	, http = require('http')
+	, server = http.createServer(app)
+	, engine = require('ejs-locals')
+	, mongoose = require('mongoose')
+    , boughtIt = require('./model/boughtIt');
+    
+app.configure(function () {
+    app.engine('ejs', engine);
+	app.use('/public', express.static(__dirname + '/public'));
+	app.set('view engine', 'ejs');
+});
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.get('/boughtit', function(req, res) {
+    boughtIt.create("pants", function(model) {
+        res.render('boughtit', model);
+    });
+});
+
+app.get('/madeit', function(req, res) {
+    res.render('madeit');
+});
+
+mongoose.connect('mongodb://coffee:coffee@ds033317.mongolab.com:33317/coffeeapp');
+
+var db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+
+db.once('open', function () {
+    app.listen(process.env.PORT);
+});
---
+++ model/boughtIt.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+var schema = mongoose.Schema({ name: 'string' });
+var boughtIt = mongoose.model('BoughtIt', schema);
+
+module.exports = {
+    create: function(name, callback) {
+        var b = new boughtIt({ name : name });
+        b.save(function() {
+            callback(b);
+            console.log("Saved some " + name);
+        });
+    }
+}
